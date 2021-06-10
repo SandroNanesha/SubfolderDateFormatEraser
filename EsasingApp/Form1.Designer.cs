e7cc6545b9bb2ec34d2acba02d28218a806361cf
SandroNanesha <56513599+SandroNanesha@users.noreply.github.com>
2021-06-10 19:09:30 +0400

    For YYMMDD format only

---
+++ EsasingApp/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace EsasingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtBoxFilePath = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtPickerFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtPickerTo = new System.Windows.Forms.DateTimePicker();
+            this.Range = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBoxFilePath
+            // 
+            this.txtBoxFilePath.Location = new System.Drawing.Point(120, 29);
+            this.txtBoxFilePath.Name = "txtBoxFilePath";
+            this.txtBoxFilePath.Size = new System.Drawing.Size(564, 20);
+            this.txtBoxFilePath.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "FilePath";
+            // 
+            // dtPickerFrom
+            // 
+            this.dtPickerFrom.Location = new System.Drawing.Point(120, 77);
+            this.dtPickerFrom.Name = "dtPickerFrom";
+            this.dtPickerFrom.Size = new System.Drawing.Size(200, 20);
+            this.dtPickerFrom.TabIndex = 3;
+            // 
+            // dtPickerTo
+            // 
+            this.dtPickerTo.Location = new System.Drawing.Point(409, 77);
+            this.dtPickerTo.Name = "dtPickerTo";
+            this.dtPickerTo.Size = new System.Drawing.Size(200, 20);
+            this.dtPickerTo.TabIndex = 3;
+            // 
+            // Range
+            // 
+            this.Range.AutoSize = true;
+            this.Range.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Range.Location = new System.Drawing.Point(12, 77);
+            this.Range.Name = "Range";
+            this.Range.Size = new System.Drawing.Size(67, 20);
+            this.Range.TabIndex = 2;
+            this.Range.Text = "FilePath";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(652, 179);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(391, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "This soft deletes every sub-folder between date range.\r\nSub-folder name format mu" +
+    "st be YYMMDD";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 249);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.dtPickerTo);
+            this.Controls.Add(this.dtPickerFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Range);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxFilePath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxFilePath;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtPickerFrom;
+        private System.Windows.Forms.DateTimePicker dtPickerTo;
+        private System.Windows.Forms.Label Range;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EsasingApp/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsasingApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DateTime from;
+        private DateTime to;
+
+
+        private void rec(string[] filesindirectory)
+        {
+            foreach (string str in filesindirectory)
+            {
+                if (filesindirectory.Length == 0) return;
+
+                if (Directory.Exists(str))
+                {
+                    string[] subFolders = Directory.GetDirectories(str);
+                    rec(subFolders);
+                    
+                    if(needToBeDeleted(str))
+                    {
+                        Directory.Delete(str, true);
+                    }
+                }
+
+
+            }
+        }
+
+        private bool needToBeDeleted(string currPath)
+        {
+
+            String currName = new DirectoryInfo(currPath).Name;
+
+
+            if (int.TryParse(currName.Substring(0, 2), out int currYear) && int.TryParse(currName.Substring(2, 2), out int currMonth) && int.TryParse(currName.Substring(4, 2), out int currDay))
+            {
+                currYear = 2000 + currYear;
+                try
+                {
+                    DateTime currOne = new DateTime(currYear, currMonth, currDay);
+
+                    if (currOne >= from && currOne <= to)
+                    {
+                        return true;
+                    }
+                } catch
+                {
+
+                }
+                
+
+                
+                
+
+            }
+            return false;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            String filePath = txtBoxFilePath.Text;
+
+            from = dtPickerFrom.Value.Date;
+
+            to = dtPickerTo.Value.Date;
+
+            if (Directory.Exists(filePath))
+            {
+                string[] filesindirectory = Directory.GetDirectories(filePath);
+
+                rec(filesindirectory);
+
+
+                MessageBox.Show("Successfully excecuted");
+
+            } else
+            {
+                MessageBox.Show("Directory does not exsist");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
